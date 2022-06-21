7212dfd82d52678bdc5dd8e5d357c1aecce565cb
rubystanley <rubystanley@gmail.com>
2022-06-21 11:18:41 -0400

    Final commit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sys
 from housing.logger import logging
 from housing.exception import HousingException
 
+
+
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
@@ -13,8 +15,8 @@ def index():
         housing = HousingException(e,sys)
         logging.info(housing.error_message)
         logging.info("We are testing logging module")
-    
-    return "CI CD Pipeline has been established"
+    return "CI CD Pipeline has been established."
+
 
 
 if __name__ == "__main__" :
--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -7,6 +7,8 @@ class HousingException(Exception):
         super().__init__(error_message)
         self.error_message=HousingException.get_detailed_error_message(error_message=error_message,error_detail=error_detail)
 
+        
+
     @staticmethod
     def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
         """
@@ -19,6 +21,8 @@ class HousingException(Exception):
         file_name=exec_tb.tb_frame.f_code.co_filename
         error_message=f"Error occured in script:[{file_name}] at line number :[{line_number}] error message: [{error_message}]"
         return error_message
+
+    
 
     def __str__(self):
         return self.error_message
